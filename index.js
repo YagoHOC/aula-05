d5f0c57297ef063b2ca0691ce8ea54ac0f74a56e
YagoHOC <108764796+YagoHOC@users.noreply.github.com>
2023-08-21 21:43:24 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,31 @@
+//comentario
+/* comentario 2
+comentário 3
+*/
+//console.log('Olá mundo')
+//prompt('Qual é o seu nome?')
+
+//const novaVariavel = 100
+//declaracao nome = valor 
+
+//const novaVariavel2 = novaVariavel + 50 
+ //console.log (novaVariavel2) 
+
+
+ //let numero = 50
+//console.log(numero)
+//numero = 80
+//console.log(numero)
+
+//const nome = 'Yago'
+//const idade = 19
+
+//console.log('Olá! Meu nome é',nome,'e eu tenho',idade,'anos ')
+
+const primeiroNome = 'Yago'
+const segundoNome = 'Henrique'
+const idade = 19 
+const souEstudante = true
+
+console.log('Meu nome é',primeiroNome,segundoNome,'tenho',idade,'anos de idade','e se eu sou estudante?',souEstudante)
+
